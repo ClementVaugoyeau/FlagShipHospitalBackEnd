2e15c44057f2725501efa025c7c688bc67d2b4b2
Shaban Hamiti <84515408+ShabanHamiti@users.noreply.github.com>
2022-02-09 10:58:54 +0100

    Api User
    Création de l'api => User
    Get, Get(ID), Post, Delete

---
+++ Models/FlagSHospitalContext.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace FlagShipHospitalBackEnd.Models
+{
+    public partial class FlagSHospitalContext : DbContext
+    {
+        public FlagSHospitalContext()
+        {
+        }
+
+        public FlagSHospitalContext(DbContextOptions<FlagSHospitalContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Dossierpatient> Dossierpatients { get; set; } = null!;
+        public virtual DbSet<User> Users { get; set; } = null!;
+
+        /*protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseNpgsql("Server=localhost;Port=5432;Database=FlagSHospital;User Id=postgres;Password=SuperMotDePasseOfDoom");
+            }
+        }*/
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Dossierpatient>(entity =>
+            {
+                entity.ToTable("dossierpatient");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.DateArrivee)
+                    .HasColumnType("timestamp without time zone")
+                    .HasColumnName("date_arrivee");
+
+                entity.Property(e => e.DateDepart)
+                    .HasColumnType("timestamp without time zone")
+                    .HasColumnName("date_depart");
+
+                entity.Property(e => e.Nom)
+                    .HasColumnType("character varying")
+                    .HasColumnName("nom");
+
+                entity.Property(e => e.Note)
+                    .HasColumnType("character varying")
+                    .HasColumnName("note");
+
+                entity.Property(e => e.Prenom)
+                    .HasColumnType("character varying")
+                    .HasColumnName("prenom");
+            });
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.ToTable("users");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Email)
+                    .HasColumnType("character varying")
+                    .HasColumnName("email");
+
+                entity.Property(e => e.Motdepasse)
+                    .HasColumnType("character varying")
+                    .HasColumnName("motdepasse");
+
+                entity.Property(e => e.Role)
+                    .HasColumnType("character varying")
+                    .HasColumnName("role");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using FlagShipHospitalBackEnd.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<FlagSHospitalContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DevConnectionPostGreSQL"));
+});
 
 var app = builder.Build();
 
