bea190b36a01060e1af8002ef134393bf182ead6
Shaban Hamiti <84515408+ShabanHamiti@users.noreply.github.com>
2022-02-10 16:02:42 +0100

    Cryptage Mot de passe

--- Models/User.cs
+++ Models/User.cs
@@ -4,11 +4,22 @@ using System.Text.Json.Serialization;
 
 namespace FlagShipHospitalBackEnd.Models
 {
-    public partial class User
+    public class User
     {
         public int Id { get; set; }
         public string Email { get; set; }
         public string Role { get; set; }
         public string Motdepasse { get; set; }
+
+        public User(int id, string email, string role, string motdepasse)
+        {
+            Id = id;
+            Email = email;
+            Role = role;
+            Motdepasse = motdepasse;
+        }
+
     }
+
+    
 }
