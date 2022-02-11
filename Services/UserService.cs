2d7ec8aa74a5a89bf55a1e316b7af8d80f3c75ea
ClementVaugoyeau <77982870+ClementVaugoyeau@users.noreply.github.com>
2022-02-11 11:21:33 +0100

    Update UserService.cs

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -1,4 +1,4 @@
-﻿using FlagShipHospitalBackEnd.Helpers;
+﻿ using FlagShipHospitalBackEnd.Helpers;
 using FlagShipHospitalBackEnd.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
